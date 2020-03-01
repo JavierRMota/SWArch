69d806171215b47700b654030fe4ac85833016ab
Javier Rodríguez Mota <A01372812@itesm.mx>
2020-02-29 22:05:27 -0600

    Documentation

--- observer/src/twitter.rb
+++ observer/src/twitter.rb
@@ -1,20 +1,38 @@
+# File name: twitter.rb
+# Observer Method Pattern
+# Date: 29-Feb-2020
+# Authors:
+#          A01372812 José Javier Rodríguez Mota
+#          A01379228 Adrián Méndez López
+
+#Twitter class
 class Twitter
+
+    #Creates a new user, with 0 followers
     def initialize(name)
         @name = name
         @followers = []
     end
+
+    #Notifies followers of the content provided
     def tweet(content)
         @followers.each do |follower|
             puts("#{follower.name} received a tweet from #{@name}: #{content}" )
         end
     end
+
+    #Calls subscribe follower of the given user with self as parameter
     def follow(user)
         user.add_follower(self)
         self
     end
+
+    #Adds follower to a given user
     def add_follower(user)
         @followers << user
     end
+
+    #Returns the name of the user
     def name
         @name
     end
--- observer/src/twitter_test.rb
+++ observer/src/twitter_test.rb
@@ -1,11 +1,18 @@
 # File name: twitter_test.rb
+# Observer Method Pattern
+# Date: 29-Feb-2020
+# Authors:
+#          A01372812 José Javier Rodríguez Mota
+#          A01379228 Adrián Méndez López
 
 require 'minitest/autorun'
 require 'stringio'
 require './twitter'
 
+#This tests uses the output of the command line
 class TwitterTest < Minitest::Test
 
+  #Initializes test instance variables
   def setup
     @out = StringIO.new
     @old_stdout = $stdout
@@ -16,24 +23,30 @@ class TwitterTest < Minitest::Test
     $stdout = @old_stdout
   end
 
+  #Test with an Alice in Wonderland Theme
   def test_twitter_alices_adventures_in_wonderland
+
+    #Initialize users
     a = Twitter.new('Alice')
     k = Twitter.new('King')
     q = Twitter.new('Queen')
     h = Twitter.new('Mad Hatter')
     c = Twitter.new('Cheshire Cat')
 
+    #Make some users follow others
     a.follow(c)
     k.follow(q)
     h.follow(a).follow(q).follow(c)
     q.follow(q)
 
+    #Tweet with some users
     a.tweet "What a strange world we live in."
     k.tweet "Begin at the beginning, and go on till you come "  \
       "to the end: then stop."
     q.tweet "Off with their heads!"
     c.tweet "We're all mad here."
 
+    #Compare expected results with the console output
     assert_equal \
       "Mad Hatter received a tweet from Alice: What a strange " \
         "world we live in.\n"                                   \
@@ -50,15 +63,19 @@ class TwitterTest < Minitest::Test
       @out.string
   end
 
+  #Test with a Star Wars Theme
   def test_twitter_star_wars
+    #Initialize users
     y = Twitter.new('Yoda')
     o = Twitter.new('Obi-Wan Kenobi')
     v = Twitter.new('Darth Vader')
     p = Twitter.new('Padmé Amidala')
 
+    #Make some users follow others
     p.follow(v)
     v.follow(p).follow(y).follow(v)
 
+    #Tweet with some users
     y.tweet "Do or do not. There is no try."
     o.tweet "The Force will be with you, always."
     v.tweet "I find your lack of faith disturbing."
@@ -67,6 +84,7 @@ class TwitterTest < Minitest::Test
     p.tweet "I will not condone a course of action that will "  \
       "lead us to war."
 
+    #Compare expected results with the console output
     assert_equal \
       "Darth Vader received a tweet from Yoda: Do or do not. "  \
         "There is no try.\n"                                    \
