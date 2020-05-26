64ec44605dd7b6ffbd7ee5e8b703ca81227271cf
Javier Rodríguez Mota <A01372812@itesm.mx>
2020-05-26 17:54:54 -0500

    Connection

--- finalproject/src/server.rb
+++ finalproject/src/server.rb
@@ -1,7 +1,21 @@
 require 'sinatra'
+require 'json'
+require 'faraday'
+
+BASE_URL = 'https://9xaawb1uu2.execute-api.us-east-1.amazonaws.com/default/'
+API_KEY = 'NMIAGNqxO5aQUeIKSWYod7TDzgowKqa5kUYTLh62'
+CONNECTION = Faraday.new(
+  url: BASE_URL,
+  headers: {
+    'Content-Type' => 'application/json',
+    'x-api-key' => API_KEY
+  }
+)
+
 def tag tag, text
   "<#{tag}>#{text}</#{tag}>"
 end
+
 get '/login' do
   @title = "Inicio de sesion"
   erb :login, :layout => :page
