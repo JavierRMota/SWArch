8d9cabd556046d9263bf03d993702f0a1350a40c
Javier Rodríguez Mota <A01372812@itesm.mx>
2020-05-31 12:27:57 -0500

    Implemented home and quiz routes

--- finalproject/src/server.rb
+++ finalproject/src/server.rb
@@ -12,11 +12,31 @@ CONNECTION = Faraday.new(
   }
 )
 
+SCORE = 'scores'
+EVALUATOR = 'evaluator'
+QUESTION = 'question'
+
 def tag tag, text
   "<#{tag}>#{text}</#{tag}>"
 end
 
 get '/login' do
-  @title = "Inicio de sesion"
+  @title = "Login"
   erb :login, :layout => :page
+end
+
+get '/' do
+  @title = "Quiz System"
+  erb :home, :layout => :page
+end
+
+get '/quiz/*/*' do |name, quiz|
+  resp = CONNECTION.get("#{SCORE}?quiz=#{quiz}&name=#{name}")
+  puts resp.body
+  resp.body
+end
+
+post '/quiz/*/*' do |name, quiz|
+ CONNECTION.post(EVALUATOR,'{"choice": "sake"}',
+  "Content-Type" => "application/json")
 end
