1192745ae5eafc83ffb41ec357f6431f21e63346
Javier Rodríguez Mota <A01372812@itesm.mx>
2020-06-01 18:15:30 -0500

    Question and solution working

--- finalproject/src/server.rb
+++ finalproject/src/server.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'json'
 require 'faraday'
+require 'faraday_middleware'
 
 BASE_URL = 'https://9xaawb1uu2.execute-api.us-east-1.amazonaws.com/default/'
 API_KEY = 'NMIAGNqxO5aQUeIKSWYod7TDzgowKqa5kUYTLh62'
@@ -10,7 +11,10 @@ CONNECTION = Faraday.new(
     'Content-Type' => 'application/json',
     'x-api-key' => API_KEY
   }
-)
+) do |faraday|
+  faraday.adapter Faraday.default_adapter
+  faraday.response :json
+end
 
 SCORE = 'scores'
 EVALUATOR = 'evaluator'
@@ -22,11 +26,16 @@ end
 
 get '/login' do
   @title = "Login"
+  @name = "login"
   erb :login, :layout => :page
 end
 
 get '/score' do
   @title = "Score"
+  @name = "score"
+  resp = CONNECTION.get(SCORE)
+  @scores = resp.body
+  puts @scores
   erb :score, :layout => :page
 end
 
@@ -37,11 +46,41 @@ end
 
 get '/quiz/*/*' do |name, quiz|
   resp = CONNECTION.get("#{SCORE}?quiz=#{quiz}&name=#{name}")
-  puts resp.body
-  resp.body
+  quizBody = resp.body
+  question = quizBody["questions"].pop()
+  questionResp = CONNECTION.get("#{QUESTION}?id=#{question}")
+  @id = questionResp.body["id"]
+  @score = quizBody["score"]
+  @question = questionResp.body["question"]
+  @A = questionResp.body["A"]
+  @B = questionResp.body["B"]
+  @C = questionResp.body["C"]
+  @D = questionResp.body["D"]
+  @name = name
+  @quiz = quiz
+  erb :question, :layout => :page
 end
 
 post '/quiz/*/*' do |name, quiz|
- CONNECTION.post(EVALUATOR,'{"choice": "sake"}',
-  "Content-Type" => "application/json")
+  postResp = CONNECTION.post(EVALUATOR) do |req|
+    req.headers['Content-Type'] = 'application/json'
+    req.body = { id: params['id'], answer: params['answer'] }.to_json
+  end
+  if postResp.status === 200
+    @question = params['question']
+    @answer = params['answer']
+    @correct = postResp.body['correct']
+    
+    @name = name
+    @quiz = quiz
+    @correctAnswer = postResp.body['answer']
+    scorePut = CONNECTION.put(SCORE) do |req|
+      req.headers['Content-Type'] = 'application/json'
+      req.body = { name: name, time: quiz, question: params['id'].to_i, answer: @correct }.to_json
+    end
+    @score = scorePut.body["score"]
+    erb :solution, :layout => :page
+  else
+    erb :error, :layout => :page
+  end
 end
