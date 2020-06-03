4cc787847a5155e2a7412ebb923d174b7999b31c
Javier Rodríguez Mota <A01372812@itesm.mx>
2020-06-03 17:06:03 -0500

    HttpStatus documented

---
+++ microservices/src/lambda/http_status.rb
@@ -0,0 +1,24 @@
+# Date: 09-Jun-2020
+# File: lambda_evaluator.rb
+# Authors: A01372812 José Javier Rodríguez Mota
+#          A01379228 Adrián Méndez López
+
+# The +HttpStatus+ class is an 
+# object containing the constants
+# for each useful HTTP response.
+class HttpStatus
+    # OK constant
+    OK = 200
+    # CREATED constant
+    CREATED = 201
+    # ACCEPTED constant
+    ACCEPTED = 202
+    # BAD_REQUEST constant
+    BAD_REQUEST = 400
+    # METHOD_NOT_ALLOWED constant
+    METHOD_NOT_ALLOWED = 405
+    # NOT_FOUND constant
+    NOT_FOUND = 404
+    # ERROR constant
+    ERROR = 500
+end
