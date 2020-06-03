43de760688f55e28d1182f16549c12cb79ec417b
Javier Rodríguez Mota <A01372812@itesm.mx>
2020-06-03 15:01:59 -0500

    Server documented

--- microservices/src/server.rb
+++ microservices/src/server.rb
@@ -9,18 +9,22 @@ require 'json'
 require './models/quiz_factory'
 require './models/quiz_exception'
 
+# QUIZ constant using QuizFactory to get the correct API endpoint and key
 QUIZ = QuizFactory.create
 
-def tag tag, text
-  "<#{tag}>#{text}</#{tag}>"
+# Returns a formatted tag in the form <tag class="classes">text</tag>
+def tag tag, text, classes = nil
+  "<#{tag} #{classes.nil ? '': "class='#{classes}'"}'>#{text}</#{tag}>"
 end
 
+# Renders login view
 get '/login' do
   @title = "Login"
   @name = "login"
   erb :login, :layout => :page
 end
 
+# Renders score view with score response from API
 get '/score' do
   @title = "Score"
   @name = "score"
@@ -28,20 +32,25 @@ get '/score' do
   erb :score, :layout => :page
 end
 
+# Renders home view
 get '/' do
   @title = "Quiz Application with Microservices"
   erb :home, :layout => :page
 end
 
+# Renders continue quiz view
 get '/continue' do
   @title = "Continue Quiz"
   erb :continue, :layout => :page
 end
 
+# Redirects to previously generated quiz
 post '/continue' do
   redirect "quiz/#{params["name"]}/#{params["quizId"]}"
 end
 
+# Loads quiz next question and renders question view
+# If a method raises an exception renders the corresponding view
 get '/quiz/*/*' do |name, quizId|
   begin
     question = QUIZ.get_next_question(name,quizId)
@@ -63,6 +72,8 @@ get '/quiz/*/*' do |name, quizId|
   end
 end
 
+# Evaluates an answer and renders the solution view
+# If a method raises an exception renders the corresponding view
 post '/quiz/*/*' do |name, quizId|
   begin
     evaluation = QUIZ.evaluate(name, quizId, params['id'], params['answer'])
@@ -81,7 +92,7 @@ post '/quiz/*/*' do |name, quizId|
   end
 end
 
-
+# Sets 404 template
 not_found do
   status 404
   @error = "Link not found"
